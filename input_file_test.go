0e40b0343217d007fcb374c87a1b3853c4f37bee
matanbaruch <matan.baruch@unity3d.com>
2025-05-22 17:26:17 +0300

    Add file watching functionality to FileInput
    - Introduced FileInputConfig to manage new configuration options for file watching.
    - Implemented watchForNewFiles and checkForNewFiles methods to monitor and process newly created files.
    - Added a new test case to validate the functionality of watching for new files.
    - Updated settings to include options for enabling file watching and setting the watch interval.

--- input_file_test.go
+++ input_file_test.go
@@ -235,6 +235,55 @@ func TestInputFileCompressed(t *testing.T) {
 	os.Remove(name2)
 }
 
+func TestInputFileWatchForNewFiles(t *testing.T) {
+	rnd := rand.Int63()
+	basePath := fmt.Sprintf("/tmp/%d", rnd)
+
+	// Create first file
+	file1, _ := os.OpenFile(fmt.Sprintf("%s_1", basePath), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0660)
+	file1.Write([]byte("1 1 1\ntest1"))
+	file1.Write([]byte(payloadSeparator))
+	file1.Close()
+
+	// Initialize input with watching enabled and short watch interval
+	config := &FileInputConfig{
+		Loop:          false,
+		ReadDepth:     100, 
+		MaxWait:       0,
+		DryRun:        false,
+		WatchNewFiles: true,
+		WatchInterval: 300 * time.Millisecond, // Faster interval for testing
+	}
+	
+	input := NewFileInputWithConfig(fmt.Sprintf("%s_*", basePath), config)
+	
+	// Read the first message
+	msg1, err := input.PluginRead()
+	if err != nil || string(msg1.Data) != "test1" {
+		t.Error("Should read first file correctly:", err)
+	}
+
+	// Add a second file while input is running
+	file2, _ := os.OpenFile(fmt.Sprintf("%s_2", basePath), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0660)
+	file2.Write([]byte("1 1 2\ntest2"))
+	file2.Write([]byte(payloadSeparator))
+	file2.Close()
+
+	// Wait for file discovery and processing (at least 2 watch intervals)
+	time.Sleep(700 * time.Millisecond)
+
+	// Should be able to read from the newly added file
+	msg2, err := input.PluginRead()
+	if err != nil || string(msg2.Data) != "test2" {
+		t.Error("Should read newly added file correctly:", err)
+	}
+
+	// Clean up
+	input.Close()
+	os.Remove(file1.Name())
+	os.Remove(file2.Name())
+}
+
 type CaptureFile struct {
 	msgs []*Message
 	file *os.File
